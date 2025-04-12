82032683e519526fbd56a7e4fa533fef8e1d1905
soli-vox <devisopen@gmail.com>
2025-04-12 22:59:47 +0545

    removed cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => RoleMiddleware::class,
-            CorsMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
