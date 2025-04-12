73cb217a83fd844ecd0b8137c87054f806a3c5dd
soli-vox <devisopen@gmail.com>
2025-04-12 22:41:15 +0545

    changes in CorsMiddleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -12,19 +12,9 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $frontendUrl = env('APP_FRONTEND_URL', 'https://pvms-test-frontend.netlify.app');
-
-        \Log::info('CORS Headers Set', [
-            'Origin' => $frontendUrl,
-            'Method' => $request->getMethod(),
-            'URL' => $request->fullUrl(),
-        ]);
-
-        $response->headers->set('Access-Control-Allow-Origin', $frontendUrl);
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
-
         return $response;
     }
 }
