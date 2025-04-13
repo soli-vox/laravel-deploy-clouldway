7b4cb69aa6c00bfa5937a7adf2bd97d65551dfe2
soli-vox <devisopen@gmail.com>
2025-04-13 10:02:16 +0545

    Uncommented the .htacess and RoleMiddleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => RoleMiddleware::class,
-
         ]);
         $middleware->statefulApi();
 
