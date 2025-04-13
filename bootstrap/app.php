2199e09162cb25fb18dd8500471a571893fda82d
soli-vox <devisopen@gmail.com>
2025-04-13 09:17:59 +0545

    cors added in the RoleMiddleware with header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Middleware\CorsMiddleware;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,8 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => RoleMiddleware::class,
-            'api' => HandleCors::class,
-            CorsMiddleware::class,
+
         ]);
         $middleware->statefulApi();
 
