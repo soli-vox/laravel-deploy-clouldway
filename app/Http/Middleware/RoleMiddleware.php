d2a3be2a717818ff6ee04a35eb936fa473c37c9d
soli-vox <devisopen@gmail.com>
2025-04-13 09:16:01 +0545

    cors added in the RoleMiddleware with header

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -14,7 +14,7 @@ class RoleMiddleware
 
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://pvms-test-frontend.netlify.app');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
 
