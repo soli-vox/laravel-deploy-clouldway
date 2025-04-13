f541d914123c55a15f5ce077913102258fc3d2e7
soli-vox <devisopen@gmail.com>
2025-04-13 19:32:35 +0545

    I know what i am doing

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -11,10 +11,13 @@ class CorsMiddleware
 
     public function handle(Request $request, Closure $next): Response
     {
+        \Log::info('CorsMiddleware hit for method: ' . $request->getMethod() . ', URL: ' . $request->url());
+
         $response = $next($request);
         $response->headers->set('Access-Control-Allow-Origin', env('FRONTEND_URL', 'http://localhost:5173'));
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Origin, Accept, Authorization');
+        $response->headers->set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+        //        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Origin, Accept, Authorization');
         $response->headers->set('Access-Control-Max-Age', 86400);
         if ($request->getMethod() === 'OPTIONS') {
             return new Response('', 204, $response->headers->all());
