db3dedafa28626f9eb4222613294eab569a8b1e7
soli-vox <devisopen@gmail.com>
2025-04-13 13:15:09 +0545

    Use of HandleCors which is append with Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,10 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => RoleMiddleware::class,
-            HandleCors::class,
         ]);
-
-
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
